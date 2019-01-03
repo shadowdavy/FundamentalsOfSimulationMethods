dcb5db431773fa32451dbea1ac0ab72f2730a9ff
David Bubeck <shadowdavy@gmail.com>
2019-01-03 12:15:10 +0100

    change vel calculation

--- Exercises_FSM/planetModel_test.cpp
+++ Exercises_FSM/planetModel_test.cpp
@@ -17,7 +17,7 @@ typedef struct {
 
 void calcAcc(object* o, int nbody) {
     double g, temp_vel1, temp_vel2;
-    for(int i = 0; i < nbody; i++) {
+    for(int i = 0; i < nbody - 1; i++) {
         /*o[i].vel_old[0] = o[i].vel[0];
         o[i].vel_old[1] = o[i].vel[1];
         o[i].vel_old[2] = o[i].vel[2];
@@ -42,35 +42,57 @@ void calcAcc(object* o, int nbody) {
         o[i + 1].pos[1] = o[i + 1].vel_old[1];
         o[i + 1].pos[2] = o[i + 1].vel_old[2];*/
 
-        for(int j = 0; j < 2; j++) {
+        for(int j = 1; j < nbody; j++) {
             if(i == j) continue;
-            temp_vel1 = o[i].vel[j]
-            temp_vel2 = o[i + 1].vel[j]
+            g = G / (std::pow(o[i].pos[0] - o[j].pos[0], 2) + std::pow(o[i].pos[1] - o[j].pos[1], 2) + std::pow(o[i].pos[2] - o[j].pos[2], 2));
+            for(int k = 0; k < 3; k++) {
+                temp_vel1 = o[i].vel[k];
+                temp_vel2 = o[j].vel[k];
+
+                o[i].vel[k] = - g * o[i].mass * (o[i].pos[k] - o[j].pos[k]);
+                o[j].vel[k] = g * o[j].mass * (o[i].pos[k] - o[j].pos[k]);
+            
+                o[i].pos[k] = temp_vel1;
+                o[j].pos[k] = temp_vel2;
+            }
+            
         }
     }
 }
 
-std::vector<double> leapfrog(function f, std::vector<double> y, double dt) {
-    std::vector<double> y_firstHalf;
-    std::vector<double> y_secondHalf;
-    std::vector<double> x_half;
-    std::vector<double> x;
-    std::vector<double> v;
-    int vecSize = y.size();
 
-    for(int i = 0; i < vecSize / 2; i++) {
-        y_firstHalf.push_back(y[i]);
+void calcVelAcc(object* o, int num, int k, int nbody) {
+    double g, temp_vel1, temp_vel2;
+    for(int i = num + 1; i < nbody; i++) {
+        g = G / (std::pow(o[num].pos[0] - o[i].pos[0], 2) + std::pow(o[num].pos[1] - o[i].pos[1], 2) + std::pow(o[num].pos[2] - o[i].pos[2], 2));
+                
+        temp_vel1 = o[num].vel[k];
+        temp_vel2 = o[i].vel[k];
+
+        o[num].vel[k] = - g * o[num].mass * (o[num].pos[k] - o[i].pos[k]);
+        o[i].vel[k] = g * o[i].mass * (o[num].pos[k] - o[i].pos[k]);
+            
+        o[num].pos[k] = temp_vel1;
+        o[i].pos[k] = temp_vel2;
     }
-    for(int i = vecSize / 2; i < vecSize; i++) {
-        y_secondHalf.push_back(y[i]);
+}
+
+
+void leapfrog(object* o, int nbody, double dt) {
+    double x_half, v, a;
+    for(int i = 0; i < nbody; i++) {
+        for(int k = 0; k < 3; k++) {
+            x_half = o[i].pos[k] + (dt / 2) * o[i].vel[k];
+            v = o[i].vel[k];
+            calcVelAcc(o, i, k, nbody);
+            o[i].vel[k] = v + dt * o[i].vel[k];
+            o[i].pos[k] = x_half + (dt / 2) * o[i].vel[k];
+        }   
     }
-    
-    for(int i = 0; i < vecSize / 2; i++) {
-        x_half.push_back(y_secondHalf[i] + (dt / 2) * y_firstHalf[i]);
-    }
-    for(int i = 0; i < vecSize / 2; i++) {
-        
-    }
-    
-    return -1;
 } 
+
+int main() {
+    
+
+    return 0;
+}
